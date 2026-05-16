2f1469ab75a9a9cd5545f97041800bd666f9c933
qiukuip <zhulongkun20@icloud.com>
2026-05-16 12:05:37 +0800

    :wrench: config go and langchain

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,7 +7,16 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
--- Disable Spell Check for Markdown(.md) file
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
 
@@ -15,4 +24,3 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = false
   end
 })
-
